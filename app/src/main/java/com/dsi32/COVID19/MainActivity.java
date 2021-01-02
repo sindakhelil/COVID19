29ee101b11b8541a72a258418a4e448dc51838a2
sindakhelil <sindakhelil1998@gmail.com>
2021-01-02 21:54:40 +0100

    affichage d'un menu dans l'app+quelque modification

--- app/src/main/java/com/dsi32/COVID19/MainActivity.java
+++ app/src/main/java/com/dsi32/COVID19/MainActivity.java
@@ -2,17 +2,21 @@ package com.dsi32.COVID19;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-/*import com.google.android.material.navigation.NavigationView;*/
+
 
 public class MainActivity extends AppCompatActivity{
     TextView TV;
@@ -21,18 +25,47 @@ public class MainActivity extends AppCompatActivity{
     TextView TV3;
     TextView TV4;
     TextView TexteV;
+    private DrawerLayout mDrawerLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
-        TexteV = findViewById(R.id.act1);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+        }
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    // set item as selected to persist highlight
+                    menuItem.setChecked(true);
+                    // close drawer when item is tapped
+                    mDrawerLayout.closeDrawers();
+
+                    // Add code here to update the UI based on the item selected
+                    // For example, swap UI fragments here
+
+                    return true;
+                });
+        Toast.makeText(this, getString(R.string.message), Toast.LENGTH_LONG).show();
+        TexteV = findViewById(R.id.act1);
         TexteV.setOnClickListener(v -> {
             Intent inte = new Intent(MainActivity.this,MainActivityConnexion.class);
             startActivity(inte);
         });
+
+
 
         TV1 = findViewById(R.id.act4);
         TV1.setOnClickListener(v -> {
@@ -74,7 +107,13 @@ public class MainActivity extends AppCompatActivity{
         signOut();
         finish();
     }
-
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
 
